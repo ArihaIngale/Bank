903912173e5c84e7a6929960d1500884c57b2bce
01fe23bca085@kletech.ac.in <arihaingale220705@gmail.com>
2025-05-14 15:35:56 +0530

    banking

---
+++ Bankcustomer.java
@@ -0,0 +1,15 @@
+class Bankcustomer {
+    String c_name;
+    String acc_num;
+    int cust_age;
+    Bankcustomer(String c_name,String acc_num,int cust_age){
+        this.c_name=c_name;
+        this.acc_num=acc_num;
+        this.cust_age=cust_age;
+    }
+    void Displaycust(){
+        System.out.println("Customer name"+c_name);
+        System.out.println("Account number"+acc_num);
+        System.out.println("Customer age"+cust_age);
+    }
+}
